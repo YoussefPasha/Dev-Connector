b9e19c187ba7d48c5064aba57915a41971ea331c
YoussefPasha <jojoyossefbasha010@gmail.com>
2020-11-01 13:15:10 +0200

    Auth Reducer & Register Action

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -3,7 +3,8 @@ import { connect } from "react-redux";
 import { setAlert } from "../../actions/alert";
 import { Link } from "react-router-dom";
 import PropTypes from "prop-types";
-const Register = ({ setAlert }) => {
+import { register } from "../../actions/auth";
+const Register = ({ setAlert, register }) => {
   const [formData, setFormData] = useState({
     name: "",
     email: "",
@@ -22,7 +23,7 @@ const Register = ({ setAlert }) => {
     if (password !== password2) {
       setAlert("Passwords do not match", "danger");
     } else {
-      console.log(formData);
+      register({ name, email, password });
     }
   };
 
@@ -50,6 +51,7 @@ const Register = ({ setAlert }) => {
             value={email}
             onChange={(e) => onChange(e)}
             name="email"
+            required
           />
           <small className="form-text">
             This site uses Gravatar so if you want a profile image, use a
@@ -87,6 +89,7 @@ const Register = ({ setAlert }) => {
 
 Register.propTypes = {
   setAlert: PropTypes.func.isRequired,
+  register: PropTypes.func.isRequired,
 };
 
-export default connect(null, { setAlert })(Register);
+export default connect(null, { setAlert, register })(Register);
--- client/src/reducers/alert.js
+++ client/src/reducers/alert.js
@@ -1,10 +1,10 @@
+/* eslint-disable import/no-anonymous-default-export */
 import { SET_ALERT, REMOVE_ALERT } from "../actions/types";
 
 const initialState = [];
 
-export default function(state = initialState, action) {
-
-    const { type, payload } = action;
+export default function (state = initialState, action) {
+  const { type, payload } = action;
 
   switch (type) {
     case SET_ALERT:
