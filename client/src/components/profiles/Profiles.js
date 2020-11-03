86835866ceeecbf905dbd9a6245a08c9a570cbcf
YoussefPasha <jojoyossefbasha010@gmail.com>
2020-11-03 12:31:03 +0200

    get all profiles with redux and add it into public route

---
+++ client/src/components/profiles/Profiles.js
@@ -0,0 +1,29 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+import React, { Fragment, useEffect } from "react";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
+import Spinner from "../layout/Spinner";
+import { getProfiles } from "../../actions/profile";
+
+const Profiles = ({ getProfiles, profile: { profiles, loading } }) => {
+  useEffect(() => {
+    getProfiles();
+  }, []);
+
+  return (
+    <div>
+      <h1>12</h1>
+    </div>
+  );
+};
+
+Profiles.propTypes = {
+  getProfiles: PropTypes.func.isRequired,
+  profile: PropTypes.object.isRequired,
+};
+
+const mapStateToProps = (state) => ({
+  profile: state.profile,
+});
+
+export default connect(mapStateToProps, { getProfiles })(Profiles);
