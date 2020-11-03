73a915815e0f25123a6c9eeae865eeb0ce79bee8
YoussefPasha <jojoyossefbasha010@gmail.com>
2020-11-03 19:05:04 +0200

    Starting On The Profile

--- client/src/components/profile/Profile.js
+++ client/src/components/profile/Profile.js
@@ -3,6 +3,7 @@ import PropTypes from "prop-types";
 import { connect } from "react-redux";
 import Spinner from "../layout/Spinner";
 import { getProfileByID } from "../../actions/profile";
+import { Link } from "react-router-dom";
 
 const Profile = ({
   getProfileByID,
@@ -13,7 +14,26 @@ const Profile = ({
   useEffect(() => {
     getProfileByID(match.params.id);
   }, [getProfileByID, match.params.id]);
-  return <div>Profile</div>;
+  return (
+    <Fragment>
+      {profile === null || loading ? (
+        <Spinner />
+      ) : (
+        <Fragment>
+          <Link to="/profiles" className="btn btn-light">
+            Back to Profiles
+          </Link>
+          {auth.isAuthenticated &&
+            auth.loading === false &&
+            auth.user._id === profile.user._id && (
+              <Link to="/edit-profile" className="btn btn-dark">
+                Edit Your Profile
+              </Link>
+            )}
+        </Fragment>
+      )}
+    </Fragment>
+  );
 };
 
 Profile.propTypes = {
