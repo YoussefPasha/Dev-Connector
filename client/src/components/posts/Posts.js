408c5926e970e4a8d4d4b50ef1182efa596c97fa
YoussefPasha <jojoyossefbasha010@gmail.com>
2020-11-07 10:33:17 +0200

    Post Reducer, Action & Initial Component

---
+++ client/src/components/posts/Posts.js
@@ -0,0 +1,22 @@
+import React, { useEffect } from "react";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
+import { getPosts } from "../../actions/post";
+import Spinner from "../layout/Spinner";
+
+const Posts = ({ getPosts, post: { posts, loading } }) => {
+  useEffect(() => {
+    getPosts();
+  }, [getPosts]);
+  return <div />;
+};
+
+Posts.propTypes = {
+  post: PropTypes.object.isRequired,
+  getPosts: PropTypes.func.isRequired,
+};
+
+const mapStateToProps = (state) => ({
+  post: state.post,
+});
+export default connect(mapStateToProps, { getPosts })(Posts);
